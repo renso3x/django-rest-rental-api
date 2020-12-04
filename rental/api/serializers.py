9027842b8d3e3dac81cd0980f1043e873cc688ad
vdr_romeoenso <vdr_romeoenso@airasia.com>
2020-12-04 17:10:02 +0800

    added pagination

--- rental/api/serializers.py
+++ rental/api/serializers.py
@@ -3,8 +3,6 @@ from . import models
 
 
 class FriendSerializer(serializers.ModelSerializer):
-    owner = serializers.HiddenField(default=serializers.CurrentUserDefault())
-
     class Meta:
         model = models.Friend
         fields = (
--- rental/rental/settings.py
+++ rental/rental/settings.py
@@ -50,6 +50,8 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.TokenAuthentication",
     ),
+    "DEFAULT_PAGINATION_CLASS": "drf_link_header_pagination.LinkHeaderPagination",
+    "PAGE_SIZE": 100,
 }
 
 MIDDLEWARE = [
